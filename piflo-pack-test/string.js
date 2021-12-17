a676d07bcdbedd441a0a353420133e5c29f55c2a
Florian PIAULT <florian.piault@mail-ecv.fr>
2021-12-17 14:43:25 +0100

    v1.0.1

---
+++ piflo-pack-test/string.js
@@ -0,0 +1,36 @@
+// make every first letter uppercase
+export const uppercase = (str) => {
+  const words = str.split(" ");
+  const result = [];
+  words.forEach((word) =>
+    result.push(word[0].toUpperCase() + word.substring(1))
+  );
+  return result;
+};
+
+// Slugity a string
+export const slufigy = (str) => {
+  str = str.replace(/^\s+|\s+$/g, "");
+
+  // Make the string lowercase
+  str = str.toLowerCase();
+
+  // Remove accents, swap ñ for n, etc
+  var from =
+    "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
+  var to =
+    "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  // Remove invalid chars
+  str = str
+    .replace(/[^a-z0-9 -]/g, "")
+    // Collapse whitespace and replace by -
+    .replace(/\s+/g, "-")
+    // Collapse dashes
+    .replace(/-+/g, "-");
+
+  return str;
+};
